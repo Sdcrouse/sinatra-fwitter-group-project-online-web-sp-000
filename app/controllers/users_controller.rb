5f47c40a2c2e76422f9e0de809cce9b3d71b51e3
sdcro <sdcrouse91@gmail.com>
2019-09-02 20:17:22 -0700

    Save personal changes to wip branch before switching branches

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,29 @@ class UsersController < ApplicationController
   #   "You are now signed up!"
   # end
 # 
-  # get '/login' do
-  #   "You are now logged in!"
-  # end
 
+# Be sure to delete all of this and replace it with the other code.
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    else
+      "Oops! Invalid credentials."
+    end
+  end
+
+  get '/logout' do
+    erb :"users/logout"
+  end
+
+  post '/logout' do
+    session.clear
+    redirect to '/'
+  end
+  # Delete everything above.
 end
